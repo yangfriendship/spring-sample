616df584fd526b66ae429c783bc55c5b65b18ac6
yangfriendship <yangfriendship@naver.com>
2023-08-01 22:39:28 +0900

    refactor: Mapstruct Bean 등록
    - Mapstruct Mapper 는 직접 빈으로 등록한다.

---
+++ api-server/src/main/java/me/youzheng/apiserver/config/AppConfig.java
@@ -0,0 +1,17 @@
+package me.youzheng.apiserver.config;
+
+import me.youzheng.apiserver.user.mapper.UserMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy(exposeProxy = true)
+public class AppConfig {
+
+    @Bean
+    public UserMapper userMapper() {
+        return UserMapper.INSTANCE;
+    }
+
+}
