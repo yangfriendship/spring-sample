6ef695b093d29b25df02b04fd7af584c86318628
yangfriendship <yangfriendship@naver.com>
2023-08-21 20:27:04 +0900

    refactor: 테스트 예외 클래스 별도 클래스로 분리
    - 기본 메시지 추가

--- core-configure/src/test/java/me/youzheng/core/security/AnnotationHttpStatusResolverTest.java
+++ core-configure/src/test/java/me/youzheng/core/security/AnnotationHttpStatusResolverTest.java
@@ -2,12 +2,8 @@ package me.youzheng.core.security;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,33 +16,13 @@ class AnnotationHttpStatusResolverTest {
         this.annotationHttpStatusResolver = new AnnotationHttpStatusResolver();
     }
 
-    static class DontHaveResponseStatusException extends RuntimeException {
 
-    }
-
-    @ResponseStatus
-    static class DontHaveSpecifiedStatusException extends RuntimeException {
-
-    }
-
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    static class HaveUnauthorizedStatusException extends RuntimeException {
-
-    }
 
     @ParameterizedTest(name = "예외 클래스에 명시된 HttpStatus 추출 테스트. {0} 예외에서 {1} 를 얻어야한다.")
-    @MethodSource("exceptionMethodSources")
+    @MethodSource("me.youzheng.core.configure.security.exception.TestException#exceptionMethodSources")
     void 예외클래스_상태코드_추출_테스트(final Exception exception, final HttpStatus expected) {
         final HttpStatus result = this.annotationHttpStatusResolver.resolve(exception);
         assertThat(result).isEqualTo(expected);
-    }
-
-    public static Stream<Arguments> exceptionMethodSources() {
-        return Stream.of(
-                Arguments.of(new DontHaveResponseStatusException(), HttpStatus.INTERNAL_SERVER_ERROR)
-                , Arguments.of(new DontHaveSpecifiedStatusException(), HttpStatus.INTERNAL_SERVER_ERROR)
-                , Arguments.of(new HaveUnauthorizedStatusException(), HttpStatus.UNAUTHORIZED)
-        );
     }
 
 }
