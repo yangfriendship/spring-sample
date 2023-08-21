d4b5ad6754e0a17a124eba8cd9bed66b55b05e20
yangfriendship <yangfriendship@naver.com>
2023-08-21 23:37:31 +0900

    refactor: DefaultExceptionHandler IOException 핸들링 추가
    - 반환 메시지를 생성하는 중 예외가 발생할 경우 500 으로 처리하고 메시지 내용은 작성하지 않는다.
    - Response 이 커밋되지 않았다면 OutputStream 을 초기화한다.

--- core-configure/src/main/java/me/youzheng/core/configure/security/DefaultExceptionHandler.java
+++ core-configure/src/main/java/me/youzheng/core/configure/security/DefaultExceptionHandler.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.youzheng.core.code.ErrorCode;
 import me.youzheng.core.security.ExceptionHttpStatusResolver;
+import me.youzheng.core.util.WebUtils;
 import me.youzheng.core.web.ApiError;
 import me.youzheng.core.web.ApiResponse;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class DefaultExceptionHandler implements ExceptionHandler {
 
@@ -26,24 +28,45 @@ public class DefaultExceptionHandler implements ExceptionHandler {
     }
 
     @Override
-    public void handle(final ServletRequest request, final ServletResponse response, final Exception exception) throws IOException {
+    public void handle(final ServletRequest request, final ServletResponse response, final Exception exception) {
         handleInternal((HttpServletRequest) request, (HttpServletResponse) response, exception);
     }
 
-    private void handleInternal(final HttpServletRequest request, final HttpServletResponse response, final Exception exception) throws IOException {
+    private void handleInternal(final HttpServletRequest request, final HttpServletResponse response, final Exception exception) {
         final HttpStatus httpStatus = this.exceptionHttpStatusResolver.resolve(exception);
         response.setStatus(httpStatus.value());
 
-        final ApiError apiError = ApiError.builder()
+        try {
+            final ApiResponse<?> apiResponse = createApiResponse(exception);
+            final JsonGenerator jsonGenerator = this.objectMapper.getFactory().createGenerator(response.getOutputStream(), JsonEncoding.UTF8);
+            this.objectMapper.writer().writeValue(jsonGenerator, apiResponse);
+        } catch (IOException ioException) {
+            writeInternalServerError(response, ioException);
+        }
+    }
+    
+    private void writeInternalServerError(final HttpServletResponse response, final IOException ioException) {
+        WebUtils.resetBuffer(response);
+        response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        ioException.printStackTrace();
+    }
+
+    private ApiResponse<?> createApiResponse(final Exception exception) {
+        final ApiError apiError = this.createApiError(exception);
+        return ApiResponse.of(false, null, apiError);
+    }
+
+    private ApiError createApiError(final Exception exception) {
+        return ApiError.builder()
                 .message(exception.getMessage())
                 .code(ErrorCode.FAIL.getCode())
                 .build();
-
-        final ApiResponse<?> apiResponse = ApiResponse.of(false, null, apiError);
-        final JsonGenerator jsonGenerator = this.objectMapper.getFactory().createGenerator(response.getOutputStream(), JsonEncoding.UTF8);
-        this.objectMapper.writer().writeValue(jsonGenerator, apiResponse);
     }
 
+    /**
+     * {@link DefaultExceptionHandler} 는 모든 예외 클래스에 대해 처리하기 때문에 {@link ExceptionHandlerComposite} 에 추가한다면
+     * List 에 가장 뒤에 위치하게 하여 사용한다.
+     */
     @Override
     public Class<? extends Throwable> getType() {
         return Exception.class;
